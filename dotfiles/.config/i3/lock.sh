fd1507e66d9a9e5d0f212f6361cbafc9d853be27
Daniil <daniilgrbic@gmail.com>
2024-11-22 16:49:06 +0100

    Update lock.sh
    - Remember brightness before lock and restore on unlock
    - Update magick convert command to work with images with different aspect ratios

--- dotfiles/.config/i3/lock.sh
+++ dotfiles/.config/i3/lock.sh
@@ -1,15 +1,17 @@
 # before locking, set keyboard to us
 setxkbmap us;  
 
+# save the current brightness, lower it to 10 to save power when locked
+CURR_BRIGHTNESS=$(brightnessctl g)
 brightnessctl set 10;
 
 # determine current resolution
 SCREEN_RESOLUTION="$(xdpyinfo | grep dimensions | sed -r 's#.*\ ([0-9]+x[0-9]+).*#\1#')";
 
 # scale wallpaper using ImageMagick and pipe it into i3lock
-convert ~/.wallpaper.png -gravity Center -scale "$SCREEN_RESOLUTION" RGB:- \
-    | i3lock --nofork --raw "$SCREEN_RESOLUTION":rgb -i /dev/stdin;
+magick ~/.wallpaper.png -gravity Center -resize $SCREEN_RESOLUTION^ RGB:- \
+    | i3lock --nofork --raw $SCREEN_RESOLUTION:rgb -i /dev/stdin;
 
-# on unlock, return kb layouts to normal
-brightnessctl set 100;
+# on unlock, restore brightness and return kb layouts to normal
+brightnessctl set $CURR_BRIGHTNESS;
 $HOME/.config/i3/keyboard.sh;
